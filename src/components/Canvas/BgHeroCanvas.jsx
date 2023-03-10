8611bac561c2fb5fe53c1baf9ebe0d91ffef7569
Andrey Esman <andreyesman.md@gmil.com>
2023-03-10 15:58:33 +0300

    hero blob animation

--- src/components/Canvas/BgHeroCanvas.jsx
+++ src/components/Canvas/BgHeroCanvas.jsx
@@ -1,7 +1,14 @@
+import {Canvas} from '@react-three/fiber';
 import React from 'react';
-
+import Blob from './Blob';
 const BgHeroCanvas = () => {
-	return <div>BgHeroCanvas</div>;
+	return (
+		<div>
+			<Canvas camera={{position: [0.0, 0.0, 0.9]}}>
+				<Blob />
+			</Canvas>
+		</div>
+	);
 };
 
 export default BgHeroCanvas;
